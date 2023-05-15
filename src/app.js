e4385e79977315276d7ec72c680725c801a6da5c
DiegoRugerio97 <diego.rugerioc97@gmail.com>
2023-05-15 13:34:17 -0600

    feat:change products.json with realistic products, finish app.js

--- src/app.js
+++ src/app.js
@@ -4,37 +4,65 @@ import ProductManager from './ProductManager.js'
 const app = express()
 const pm = new ProductManager("./src/JSON/products.json")
 
+// Documentation (?)
+app.get("/", async (req, res) => {
+    let message = `/products - Return complete array of products
+    /products?limit=X - Return X elements of products
+    /products/<ID> - Return the product with specified ID`
+    res.send(message)
+})
+
+// /products - Entire array
+// /products?limit=X - X elements
 app.get("/products", async (req, res) => {
     let productsArray = []
     try {
         productsArray = await pm.getProducts()
     }
     catch (error) {
-        res.send(error)
+        return res.send({ "error": error })
     }
     if (!req.query.limit) {
-        res.send({"products": productsArray})
+        return res.send({ "products": productsArray })
     }
     let limit = req.query.limit
-    let slicedArray = productsArray.slice(0,limit)
-    res.send({"products": slicedArray})
+    let slicedArray = productsArray.slice(0, limit)
+    return res.send({ "products": slicedArray })
 })
 
-app.get("/products/:pid", async (req, res) => {
+// /products/<ID>
+app.get("/products/:pId", async (req, res) => {
     try {
-        const id = parseInt(req.params.pid)
+        const id = parseInt(req.params.pId)
         const product = await pm.getProductById(id)
-        res.send(product)
+        return res.send(product)
     }
     catch (error) {
-        res.send(error)
+        return res.send({ "error": error })
     }
 })
 
+const PORT = 8080
+app.listen(PORT, () => console.log("Servidor en puerto 8080"))
 
-app.listen(8000, () => console.log("Servidor en puerto 8000"))
-
+// PROMESAS
 // app.get("/productsP", (req, res) => {
-//     pm.getProducts()
-//         .then((productsArray) => res.send({ "products": productsArray }))
+//     if (!req.query.limit) {
+//         pm.getProducts()
+//             .then((productsArray) => res.send({ "products": productsArray }))
+//             .catch(error => res.send({ "error": error }))
+//     }
+//     else {
+//         let limit = req.query.limit
+//         pm.getProducts()
+//             .then((productsArray) => res.send({ "products": productsArray.slice(0, limit) }))
+//             .catch(error => res.send({ "error": error }))
+//     }
+// })
+
+// app.get("/productsP/:pId", (req, res) => {
+//     const id = parseInt(req.params.pId)
+//     pm.getProductById(id)
+//         .then((product) => res.send(product))
+//         .catch(error => res.send({ "error": error }))
 // })
